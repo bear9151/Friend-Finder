11e8afc2cc30b6f27f8f7fee0f6919ab74129152
bear9151 <barrett.allen@icloud.com>
2017-05-01 23:24:35 -0400

    Committing working changes

---
+++ app/data/friends.js
@@ -0,0 +1,79 @@
+var friendsArray = [
+    {
+        name: "Paulie Calafiore",
+        photo: "http://wwwimage4.cbsstatic.com/thumbnails/photos/w170-h215/cast/bb18_paulie_calafiore_800x1000_0.jpg",
+        scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1]
+    },
+    {
+        name: "Da'Vonne Rogers",
+        photo: "http://wwwimage5.cbsstatic.com/thumbnails/photos/w170-h215/cast/bb18_davonne_rogers_800x1000.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            2,
+            4,
+            1,
+            2,
+            3,
+            3,
+            5]
+    },
+    {
+        name: "James Huling",
+        photo: "http://wwwimage4.cbsstatic.com/thumbnails/photos/w170-h215/cast/bb18_james_huling_800x1000.jpg",
+        scores: [
+            2,
+            3,
+            3,
+            1,
+            4,
+            3,
+            4,
+            5,
+            5,
+            5]
+    },
+    {
+        name: "Nicole Franzel",
+        photo: "http://wwwimage3.cbsstatic.com/thumbnails/photos/w170-h215/cast/bb18_nicole_franzel_800x1000.jpg",
+        scores: [
+            2,
+            3,
+            4,
+            4,
+            5,
+            4,
+            4,
+            2,
+            3,
+            1]
+    },
+    {
+        name: "Bridgette Dunning",
+        photo: "http://wwwimage4.cbsstatic.com/thumbnails/photos/w170-h215/cast/bb18_bridgette_dunning_800x1000_0.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            2,
+            4,
+            1,
+            1,
+            1,
+            4,
+            5]
+    }
+];
+
+module.exports = friendsArray;
